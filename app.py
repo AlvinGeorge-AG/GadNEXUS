da296f8d69b2108ada01527817f994ed67ae757b
Alvin <alvingeorge_@outlook.com>
2025-06-08 18:08:45 +0530

    Added Admin Page With MFA verification as email otp

--- app.py
+++ app.py
@@ -4,8 +4,9 @@ from pymongo import MongoClient
 from werkzeug.security import check_password_hash , generate_password_hash
 from dotenv import load_dotenv
 from datetime import datetime
+from email.mime.text import MIMEText
 from bson import ObjectId
-import os
+import os , random , smtplib
 
 now = datetime.now()
 app = Flask(__name__)
@@ -13,17 +14,21 @@ load_dotenv()
 #for hiding secert details from public
 app.secret_key = os.getenv("key")
 MONGO_URL = os.getenv("MONGO_URL")
+adminuser = os.getenv("ADMIN_USER")
+adminpass = os.getenv("ADMIN_PASS")
+apppass = os.getenv("APP_PASS")
 # defining variable's for user interaction 
 flag = False
 boo = 0
 notboo = 0
 loginerror=0
-
+global adminbool
 #connecting app to Mongodb Atlas
 client = MongoClient(MONGO_URL)
 db = client["gadnexus"]
 users = db["users"]
 posts= db["posts"]
+
 
 
 #defining the needed route's
@@ -39,7 +44,6 @@ def index():
         notboo = 1
     return render_template("index.html",data=data,boo=boo,notboo=notboo)
 
-
 @app.route("/login",methods=["POST","GET"])
 def login():
     if(request.method=="POST"):
@@ -52,7 +56,6 @@ def login():
         else:
             return render_template("error.html",error = "Username or Password is Incorrect !",loginerror=1)
     return render_template("login.html", flag = False)
-
 
 @app.route("/register",methods=["POST","GET"])
 def reg():
@@ -70,8 +73,6 @@ def reg():
         else:
             return render_template("register.html") 
     
-
-
 @app.route("/dashboard")
 def dash():
     
@@ -80,7 +81,6 @@ def dash():
         return render_template("dashboard.html",user=session["username"],data=data)
     else:
             return render_template("error.html",error="Please Log In !")
-
 
 @app.route("/dashboard/delete",methods=["POST"])
 def delete():
@@ -120,7 +120,6 @@ def logout():
     else:
         return render_template("error.html",error="Please Log In First !")
 
-
 @app.route("/reset", methods=["POST","GET"])
 def reset():
      if(request.method=="POST"):
@@ -138,8 +137,6 @@ def reset():
             return render_template("error.html",error="Email or Username Not Found !")  
      else:     
         return render_template("reset.html")
-
-
 
 @app.route("/dashboard/upload",methods=["POST","GET"])
 def post_upload():
@@ -159,6 +156,61 @@ def post_upload():
     else:
             return render_template("error.html",error="Please Log In !")
 
+@app.route("/admin",methods=["POST","GET"])
+def admin():
+    if(request.method=="POST"):    
+        email = request.form.get("email")
+        password = request.form.get("password")
+        if (email==adminuser and password==adminpass):
+            otp = random.randint(100000,999999)
+            session["otp"]=str(otp)
+            send_otp(email,otp)
+            return render_template("verify-otp.html")
+        else:
+            return render_template("error.html",error="You Are Not An Admin !") 
+    else:       
+        return render_template("admin.html")
+    
+@app.route("/verify-otp",methods=["POST","GET"])
+def otp():
+    if(request.method=="POST"):
+        otp = request.form.get('otp')
+        if(otp==session["otp"]):
+            global adminbool
+            adminbool = 1
+            return redirect("/admin-page")
+        else:
+            return render_template("verify-otp.html", flag=True,message="Incorrect OTP , Please try Again !")
+    else:
+        return render_template("verify-otp.html")    
+
+@app.route("/admin-page")
+def adpag():
+    if(adminbool):
+        data = list(posts.find())
+        return render_template("admin-page.html",data=data)
+    else:
+        return render_template("error.html",error="Please Sign In as an Admin !")
+
+def send_otp(receiver_email,otp):
+    sender = adminuser
+    app_pass = apppass
+    message = MIMEText(f"Your ADMIN OTP Code For GadNEXUS Is: {otp} , Please Dont Reply To This Message .")
+    message['Subject'] = "Your Admin OTP Code"
+    message['From'] = sender
+    message['To'] = receiver_email
+
+    try:
+        # Connect to Gmail SMTP server
+        server = smtplib.SMTP('smtp.gmail.com', 587)  
+        server.starttls()  # Enable TLS encryption
+        server.login(sender, app_pass)
+        server.sendmail(sender, receiver_email, message.as_string())
+        server.quit()
+        return True
+    except Exception as e:
+        print("Error sending email:", e)
+        return False
 
 
 #for debugging
